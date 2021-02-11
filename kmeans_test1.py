c0798a9ddb75bc95da468414be7a0278873516a2
CutyDog <72608417+CutyDog@users.noreply.github.com>
2021-02-12 00:18:09 +0900

    Create kmeans_test1.py

---
+++ kmeans_test1.py
@@ -0,0 +1,21 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import kmeans
+
+np.random.seed(0)
+points1 = np.random.randn(50, 2)
+points2 = np.random.randn(50, 2) + np.array([5, 0])
+points3 = np.random.randn(50, 2) + np.array([5, 5])
+
+points = np.r_[points1, points2, points3]
+np.random.shuffle(points)
+
+model = kmeans.KMeans(3)
+model.fit(points)
+
+markers = ["+", "*", "o"]
+for i in range(3):
+    p = points[model.labels_ == i, :]
+    plt.scatter(p[:, 0], p[:, 1], color="k", marker=markers[i])
+
+plt.show()
